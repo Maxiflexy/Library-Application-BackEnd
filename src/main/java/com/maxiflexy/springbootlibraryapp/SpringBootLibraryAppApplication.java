d7a2a8b16936a35bc8c67cfe465b29208a47b247
Maxiflexy <onyemax247@gmail.com>
2024-07-08 01:31:23 +0100

    Initial commit

---
+++ src/main/java/com/maxiflexy/springbootlibraryapp/SpringBootLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.maxiflexy.springbootlibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLibraryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxiflexy/springbootlibraryapp/SpringBootLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiflexy.springbootlibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
