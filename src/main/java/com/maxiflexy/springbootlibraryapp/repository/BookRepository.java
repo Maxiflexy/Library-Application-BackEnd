3bbe4ad419bc4f782af005888b25d909c7067816
Maxiflexy <onyemax247@gmail.com>
2024-07-08 21:14:27 +0100

    added findByCategory in repo interface

--- src/main/java/com/maxiflexy/springbootlibraryapp/repository/BookRepository.java
+++ src/main/java/com/maxiflexy/springbootlibraryapp/repository/BookRepository.java
@@ -1,7 +1,14 @@
 package com.maxiflexy.springbootlibraryapp.repository;
 
 import com.maxiflexy.springbootlibraryapp.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam ("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
