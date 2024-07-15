cc2e065587fa3aed956da1d41189015034449a12
Maxiflexy <onyemax247@gmail.com>
2024-07-16 00:40:07 +0100

    created Review controller

---
+++ src/main/java/com/maxiflexy/springbootlibraryapp/controller/ReviewController.java
@@ -0,0 +1,27 @@
+package com.maxiflexy.springbootlibraryapp.controller;
+
+import com.maxiflexy.springbootlibraryapp.payloads.request.ReviewRequest;
+import com.maxiflexy.springbootlibraryapp.service.ReviewService;
+import com.maxiflexy.springbootlibraryapp.utils.ExtractJWT;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value = "Authorization")
+                               String token, @RequestBody ReviewRequest reviewRequest) throws Exception{
+
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if(userEmail == null){
+            throw new Exception("User email is missing");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
