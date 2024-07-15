b351aab07480d9ac184a027eeee079834c121d5e
Maxiflexy <onyemax247@gmail.com>
2024-07-15 19:25:01 +0100

    created book controller

---
+++ src/main/java/com/maxiflexy/springbootlibraryapp/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.maxiflexy.springbootlibraryapp.controller;
+
+import com.maxiflexy.springbootlibraryapp.entity.Book;
+import com.maxiflexy.springbootlibraryapp.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception{
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+
+}
