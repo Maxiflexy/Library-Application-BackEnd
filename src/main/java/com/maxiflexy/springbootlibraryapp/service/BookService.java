9977302434d066d8fb8675fb056239ef1f555c6e
Maxiflexy <onyemax247@gmail.com>
2024-07-15 19:20:40 +0100

    updated book service

--- src/main/java/com/maxiflexy/springbootlibraryapp/service/BookService.java
+++ src/main/java/com/maxiflexy/springbootlibraryapp/service/BookService.java
@@ -1,4 +1,8 @@
 package com.maxiflexy.springbootlibraryapp.service;
 
+import com.maxiflexy.springbootlibraryapp.entity.Book;
+
 public interface BookService {
+
+    Book checkoutBook(String userEmail, Long bookId) throws Exception;
 }
