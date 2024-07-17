151d8de7f9097bb04358ff8c18bf3d286cabe513
Maxiflexy <onyemax247@gmail.com>
2024-07-17 23:42:44 +0100

    created payment controller

--- src/main/java/com/maxiflexy/springbootlibraryapp/controller/PaymentController.java
+++ src/main/java/com/maxiflexy/springbootlibraryapp/controller/PaymentController.java
@@ -1,10 +1,14 @@
 package com.maxiflexy.springbootlibraryapp.controller;
 
+import com.maxiflexy.springbootlibraryapp.payloads.request.PaymentInfoRequest;
 import com.maxiflexy.springbootlibraryapp.service.PaymentService;
+import com.maxiflexy.springbootlibraryapp.utils.ExtractJWT;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("https://localhost:3000")
 @RestController
@@ -13,6 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
     private final PaymentService paymentService;
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoRequest paymentInfoRequest)
+                                        throws StripeException{
+
+        PaymentIntent paymentIntent = paymentService.createPaymentIntent(paymentInfoRequest);
+        String paymentString = paymentIntent.toJson();
+        System.out.println(paymentIntent);
+
+        return new ResponseEntity<>(paymentString, HttpStatus.OK);
+    }
+
+    @PutMapping("/payment-complete")
+    public ResponseEntity<String> stripePaymentComplete(@RequestHeader(value = "Authorization") String token)
+                                        throws Exception{
+
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if(userEmail == null){
+            throw new Exception("User email is missing");
+        }
+
+        return paymentService.stripePayment(userEmail);
+    }
 
 
 }
