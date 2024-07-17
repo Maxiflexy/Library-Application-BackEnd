93d0f70d8180f55f156103b8bc43f91a32cf75a3
Maxiflexy <onyemax247@gmail.com>
2024-07-17 23:08:20 +0100

    created payment entity

---
+++ src/main/java/com/maxiflexy/springbootlibraryapp/entity/Payment.java
@@ -0,0 +1,24 @@
+package com.maxiflexy.springbootlibraryapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userEmail;
+
+    private double amount;
+}
