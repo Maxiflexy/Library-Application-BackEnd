48272775c0cf4cd0f0046cc4bdf651c528c1a63b
Maxiflexy <onyemax247@gmail.com>
2024-07-19 23:24:58 +0100

    payment controller

--- src/main/java/com/maxiflexy/springbootlibraryapp/controller/PaymentController.java
+++ src/main/java/com/maxiflexy/springbootlibraryapp/controller/PaymentController.java
@@ -26,6 +26,7 @@ public class PaymentController {
         String paymentString = paymentIntent.toJson();
         System.out.println(paymentIntent);
 
+
         return new ResponseEntity<>(paymentString, HttpStatus.OK);
     }
 
